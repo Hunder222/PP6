cbb1ac5a36b0e9ff54e9f450cf0ba9c39cdc43a9
chri77r8 <christofferbzh@gmail.com>
2025-12-02 20:44:54 +0100

    lavet charts

--- client.js
+++ client.js
@@ -2,10 +2,9 @@ console.log(EKdataset); // local dataset from EKDatasetLocal.js imported via ind
 
 ////// import and DOM //////
 const subSections = document.querySelectorAll('.subSections');
-
-
-
-
+const lineChartCanvas = document.querySelectorAll('#genderChart1');
+const pieChartCanvas = document.querySelectorAll('#genderChart2');
+const barChartCanvas = document.querySelectorAll('#genderChart3');
 
 
 
@@ -13,9 +12,41 @@ const subSections = document.querySelectorAll('.subSections');
 
 
 ////// charts //////
+let lineChart = new Chart(lineChartCanvas, {
+    type: 'line',
+    data: {
+        labels: [], //navn på uddannelse
+        datasets: [{
+            label: '',
+            data: [], //kvotient
+            backgroundColor: '', //brug chart farver fra docs
+        }]
+    }
+})
 
+let pieChart = new Chart(pieChartCanvas, {
+    type: 'pie',
+    data: {
+        labels: ["Kønsfordeling på uddannelser"],
+        datasets: [{
+            label: ["Mand", "Kvinde"], //procentvis kønsfordeling
+            data: [], //updateChart
+            backgroundColor: '', //brug chart farver fra docs
+        }]
+    }
+})
 
-
+let barChart = new Chart(barChartCanvas, {
+    type: 'bar',
+    data: {
+        labels: [], //uddannelser
+        datasets: [{
+            label: [], // uddannelsesnavn
+            data: [], //updateChart
+            backgroundColor: '', //brug chart farver fra docs
+        }]
+    }
+})
 
 
 
