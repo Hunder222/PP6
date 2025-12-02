948d434de199d604b7491267c6651353736fee51
MrPandahs <sebastianherler@gmail.com>
2025-12-02 16:11:40 +0100

    Updated data

--- ThirdPartyData.js
+++ ThirdPartyData.js
@@ -5,7 +5,24 @@ const educationData = [
         "startingSalary": 37.600,
         "averageSalary": 42.100,
         "highestSalary": 48.000,
-        "": "",
+        "reasonsForApplying": [
+            {
+                "answer": "Jeg søgte og blev ansat efter et stillingsopslag",
+                "percent": 30
+            },
+            {
+                "answer": "Jeg fik job gennem mit netværk",
+                "percent": 17
+            },
+            {
+                "answer": "Jeg fortsatte i job på den arbejdsplads, hvor jeg skrev opgave / projekt / speciale eller var i praktik",
+                "percent": 17
+            },
+            {
+                "answer": "Jeg fortsatte i job på den arbejdsplads, hvor jeg havde studiejob",
+                "percent": 17
+            }
+        ],
         "unemploymentNewGraduate": 2,
     },
 
@@ -15,18 +32,33 @@ const educationData = [
         "startingSalary": 31.950,
         "averageSalary": 36.300,
         "highestSalary": 43.600,
-        "": "",
+        "reasonsForApplying": [
+            {
+                "answer": "Jeg søgte og blev ansat efter et stillingsopslag",
+                "percent": 52
+            },
+            {
+                "answer": "Jeg fik job gennem mit netværk",
+                "percent": 20
+            },
+            {
+                "answer": "Jeg fortsatte i job på den arbejdsplads, hvor jeg skrev opgave / projekt / speciale eller var i praktik",
+                "percent": 14
+            },
+            {
+                "answer": "Jeg fortsatte i job på den arbejdsplads, hvor jeg havde studiejob",
+                "percent": 7
+            }
+        ],
         "unemploymentNewGraduate": 5,
     },
 
     {
         "id": "ITAR",
         "name": "IT-Arkitektur",
-        "startingSalary": null,
-        "averageSalary": null,
-        "highestSalary": null,
-        "": "",
-        "unemploymentNewGraduate": null,
+        "startingSalary": 45.000,
+        "averageSalary": 67.000,
+        "highestSalary": 70.000,
     },
 
     {
@@ -35,7 +67,24 @@ const educationData = [
         "startingSalary": 28.000,
         "averageSalary": 34.200,
         "highestSalary": 39.300,
-        "": "",
+        "reasonsForApplying": [
+            {
+                "answer": "Jeg søgte og blev ansat efter et stillingsopslag",
+                "percent": 39
+            },
+            {
+                "answer": "Jeg fik job gennem mit netværk",
+                "percent": 22
+            },
+            {
+                "answer": "Jeg fortsatte i job på den arbejdsplads, hvor jeg skrev opgave / projekt / speciale eller var i praktik",
+                "percent": 22
+            },
+            {
+                "answer": "Jeg fortsatte i job på den arbejdsplads, hvor jeg havde studiejob",
+                "percent": 17
+            }
+        ],
         "unemploymentNewGraduate": 5,
     },
 
@@ -45,7 +94,24 @@ const educationData = [
         "startingSalary": 19.400,
         "averageSalary": 26.600,
         "highestSalary": 32.800,
-        "": "",
+        "reasonsForApplying": [
+            {
+                "answer": "Jeg søgte og blev ansat efter et stillingsopslag",
+                "percent": 25
+            },
+            {
+                "answer": "Jeg fik job gennem mit netværk",
+                "percent": 23
+            },
+            {
+                "answer": "Jeg fortsatte i job på den arbejdsplads, hvor jeg skrev opgave / projekt / speciale eller var i praktik",
+                "percent": 23
+            },
+            {
+                "answer": "Jeg fortsatte i job på den arbejdsplads, hvor jeg havde studiejob",
+                "percent": 15
+            }
+        ],
         "unemploymentNewGraduate": 8,
     },
 
@@ -56,7 +122,7 @@ const educationData = [
         "startingSalary": null,
         "averageSalary": null,
         "highestSalary": null,
-        "": "",
+        "reasonsForApplying": "",
         "unemploymentNewGraduate": null,
     },
 
@@ -66,7 +132,24 @@ const educationData = [
         "startingSalary": 30.150,
         "averageSalary": 36.050,
         "highestSalary": 41.300,
-        "": "",
+        "reasonsForApplying": [
+            {
+                "answer": "Jeg søgte og blev ansat efter et stillingsopslag",
+                "percent": 56
+            },
+            {
+                "answer": "Jeg fik job gennem mit netværk",
+                "percent": 17
+            },
+            {
+                "answer": "Jeg fortsatte i job på den arbejdsplads, hvor jeg skrev opgave / projekt / speciale eller var i praktik",
+                "percent": 14
+            },
+            {
+                "answer": "Jeg fortsatte i job på den arbejdsplads, hvor jeg havde studiejob",
+                "percent": 6
+            }
+        ],
         "unemploymentNewGraduate": 6,
     }
 
